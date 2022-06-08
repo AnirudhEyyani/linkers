1ed2114dc002c29ef5b38c1e3bdaa3686743f907
AnirudhEyyani <anirudheyyani@outlook.com>
2022-06-08 19:28:57 +0530

    first commit

---
+++ src/assets/items.js
@@ -0,0 +1,83 @@
+import website from '../assets/website.png'
+import developer from '../assets/developer.png'
+import design from '../assets/design.png'
+import instagram from '../assets/instagram.png'
+import linkedin from '../assets/linkedin.jpg'
+import youtube from '../assets/youtube.jpeg'
+import dribbble from '../assets/dribbble.png'
+import github from '../assets/github.png'
+import telegram from '../assets/telegram.png'
+import twitter from '../assets/twitter.png'
+import behance from '../assets/behance.png'
+import figma from '../assets/figma.png'
+import acm from '../assets/acm.png'
+
+const items = [{
+    "title": "Developer Portfolio",
+    "subtitle": "Look at my work!",
+    "image": developer,
+    "link": "https://anirudheyyani.github.io/Portfolio/" //your personal website or portfolio  link
+},
+{
+    "title": "Design Portfolio",
+    "subtitle": "LAUNCHING SOON!",
+    "image": design,
+    "link": "https://anirudheyyani.github.io/design/" //your personal website or portfolio  link
+},
+{
+    "title": "GitHub",
+    "subtitle": "@AnirudhEyyani | 🏡 to all my projects",
+    "image": github,
+    "link": "https://github.com/AnirudhEyyani" //Github Profile link
+},
+{
+    "title": "Instagram",
+    "subtitle": "@anirudheyyani_ |  Shots of my life 🙂",
+    "image": instagram,
+    "link": "https://instagram.com/anirudheyyani_" //instagram profile link 
+},
+{
+    "title": "Twitter",
+    "subtitle": "@AnirudhEyyani | 😉",
+    "image": twitter,
+    "link": "https://twitter.com/AnirudhEyyani"// twitter profile link 
+},
+{
+    "title": "LinkedIn",
+    "subtitle": "Know more about my profession 👨‍💼",
+    "image": linkedin,
+    "link": "https://www.linkedin.com/in/anirudheyyani" // linkedin
+},
+{
+    "title": "YouTube",
+    "subtitle": "Anirudh Eyyani | Just hackathon submissions here",
+    "image": youtube,
+    "link": "https://www.youtube.com/channel/UCa1TjLX4tehqvJremep6VeQ"//youtube channel link 
+},
+{
+    "title": "Figma Masterclass",
+    "subtitle": "in collaboration with RAIT ACM Student Chapter",
+    "image": figma,
+    "link": "https://youtu.be/MbWFbXKYoxg"
+},
+{
+    "title": "Graphic Designing Basics",
+    "subtitle": "in collaboration with RAIT ACM Student Chapter",
+    "image": acm,
+    "link": "https://youtu.be/kzJBHiIBpfA"
+},
+{
+    "title": "Dribbble",
+    "subtitle": "@Anirudheyyani2002 | still underway 🙃 ",
+    "image": dribbble,
+    "link": "https://dribbble.com/Anirudheyyani2002" // Dribbble profile link 
+},
+{
+    "title": "Behance",
+    "subtitle": "@anirudheyyani1 | also underway 🤌🏻 ",
+    "image": behance,
+    "link": "https://www.behance.net/anirudheyyani1" //Behance Pofile 
+}
+]
+
+export default items
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../css/skeleton.css'
+import '../css/normalize.css'
+import '../css/components.css'
+
+function Footer() {
+    return (
+        <div className="Footer container">
+            <p>Made with <span class="heart">❤️</span> by <a href={"https://twitter.com/AnirudhEyyani"}>Anirudh Eyyani</a></p>
+        </div>
+    )
+}
+
+export default Footer
